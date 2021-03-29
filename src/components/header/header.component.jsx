83109b8a906103423b029e02a2ad606e1fff0f9c
lee2020 <liping.ma36@gmail.com>
2021-03-29 10:42:09 -0400

    updating our app to redirect the user to homepage if signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,40 +5,31 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from "react-redux";
 
-const Header = ({currentUser}) => {
-    return (
-        <div className = 'header'>
-
-            <Link className='logo-container' to='/'>
-                <Logo className ='logo'/>
-            </Link>
-
-            <div className='options'>
-
-                <Link className='option' to ='/shop'>
-                    SHOP
-                </Link>
-
-                <Link className='option' to='/shop'>
-                    CONTACT
-                </Link>
-
-                {currentUser?(
-                    <div className ='option' onClick ={()=>auth.signOut()}>
-                        SIGN OUT
-                    </div>
-                ):(
-                    <Link className ='option' to = '/signin'>
+const Header = ({ currentUser }) => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+      </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+      </Link>
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                    <Link className='option' to='/signin'>
                         SIGN IN
                     </Link>
                 )}
-
-            </div>
-
         </div>
-    );
+    </div>
+);
 
-}
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
 });
